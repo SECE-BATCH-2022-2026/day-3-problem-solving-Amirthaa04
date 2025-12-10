d5408c7b424f067ddd6fe74ad8351749cb5d9481
Amirthaa04 <amirthaswaathini.a2022cse@sece.ac.in>
2025-12-10 09:59:42 +0530

    Program added

--- SortedArray.java
+++ SortedArray.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-class Main {
+class SortedArray {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
